f5dd2292fdea8a88f55d3d0ac7a8a49bafdea7a4
Qinsi ZHU <qszhu2006@gmail.com>
2024-05-30 18:16:37 +0800

    add exports

--- nimleetcode.nimble
+++ nimleetcode.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.0"
+version       = "0.1.1"
 author        = "Qinsi ZHU"
 description   = "LeetCode API in Nim"
 license       = "MIT"
--- src/nimleetcode.nim
+++ src/nimleetcode.nim
@@ -1,7 +1,9 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+import nimleetcode/[
+  jwts,
+  lcClient,
+  lcSession,
+  pageData,
+  types,
+]
 
-proc add*(x, y: int): int =
-  ## Adds two numbers together.
-  return x + y
+export jwts, lcClient, lcSession, pageData, types
